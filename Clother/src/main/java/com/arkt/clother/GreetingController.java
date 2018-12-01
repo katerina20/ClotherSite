3a6eca476167794206c11e0a1aa0b3a22344a762
Arthur <belunov.artur@gmail.com>
2018-12-02 01:11:19 +0200

    Created initial project

--- Clother/src/main/java/com/arkt/clother/GreetingController.java
+++ Clother/src/main/java/com/arkt/clother/GreetingController.java
@@ -1,4 +1,4 @@
-package arkt.co.Clother;
+package com.arkt.clother;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,12 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-
     @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
-
 }
